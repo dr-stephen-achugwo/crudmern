29b720385da2d8c29b9b63d426e1452fc7bef169
Stephen Achugwo <dr_stephen@aol.com>
2025-05-27 08:05:28 +0100

    Update tutorial.service.js

--- frontend/src/services/tutorial.service.js
+++ frontend/src/services/tutorial.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 const http = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://crudserver-rixt.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
